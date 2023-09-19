e34b55b18c1577709949d492a7aafa2e8a290214
NivedithaSwaminathan <65695299+NivedithaSwaminathan@users.noreply.github.com>
2023-09-18 23:24:08 -0600

    Update ContainsDuplicate.py

--- Arrays & Hashing/ContainsDuplicate.py
+++ Arrays & Hashing/ContainsDuplicate.py
@@ -1,13 +1,12 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
 
+        # hashset has O(1) lookup time
         hashset = set()
 
         for num in nums:
-
             if num in hashset:
                 return True
-
             hashset.add(num)
 
         return False

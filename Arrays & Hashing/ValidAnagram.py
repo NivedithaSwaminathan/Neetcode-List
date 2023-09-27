3467fa8d32b5e4c849abfcfd2dee6db8e6bb7c51
NivedithaSwaminathan <65695299+NivedithaSwaminathan@users.noreply.github.com>
2023-09-26 23:39:40 -0600

    Update ValidAnagram.py

--- Arrays & Hashing/ValidAnagram.py
+++ Arrays & Hashing/ValidAnagram.py
@@ -1,3 +1,9 @@
+# A string s is an anagram of string t is all the letters of s are in t.
+# Our first check is to verify the string lengths.
+# We create one hashmap whose indices are used to keep track of letters and we update the count of s.
+# We traverse through t and decrement the count of the letters.
+# For s to be an anagram of t, the hashmap should be all zeros. If not, we return false.
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
 
@@ -7,14 +13,12 @@ class Solution:
         hashmap =[0] * 26
 
         for ch in s:
-
             hashmap[ord(ch) - ord('a')] += 1
 
         for ch in t:
             hashmap[ord(ch) - ord('a')] -= 1
 
         for num in hashmap:
-
             if num:
                 return False
 

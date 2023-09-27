b273f98f0e396298fbdafe7a39eee9e200304919
NivedithaSwaminathan <65695299+NivedithaSwaminathan@users.noreply.github.com>
2023-09-26 23:15:46 -0600

    Update ContainsDuplicate.py

--- Arrays & Hashing/ContainsDuplicate.py
+++ Arrays & Hashing/ContainsDuplicate.py
@@ -1,7 +1,11 @@
+# We use a hashset to keep track of duplicates, if any. 
+# The lookup time for a hashset is O(1), as opposed to a hashmap which is O(n). 
+# Traverse the array and add to the hashset if it doesn't already exist. If it does, a duplicate has been found.
+
+
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
 
-        # hashset has O(1) lookup time
         hashset = set()
 
         for num in nums:

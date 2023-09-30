cda7e09388d04f57ef4956941654234be8867f0c
NivedithaSwaminathan <65695299+NivedithaSwaminathan@users.noreply.github.com>
2023-09-30 12:21:16 -0600

    Update LengthOfLastWord.py

--- Arrays & Hashing/LengthOfLastWord.py
+++ Arrays & Hashing/LengthOfLastWord.py
@@ -1,17 +1,19 @@
+# TO FIND THE LENGTH OF THE LAST WORD IN THE STRING
+# We traverse the string from the last character, using a flag to keep track of the first whitespace encountered.
+# If the whitespace is found before any word, then the flag is set.
+# We decrement the pointer until a word is found, then we find the length of that word until the next whitespace.
+
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
 
        wordlen = 0
 
        i = len(s) - 1
-
        while i >=0:
 
            if s[i] == " " and wordlen ==0:
                i -= 1
-
            else:
-
                if s[i] == " ":
                    return wordlen
 
